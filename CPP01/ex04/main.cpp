d9c2e86aa32d0bf08876389a031b6ea3efb9674b
vasekva <vahonin_vasek@mail.ru>
2022-01-07 15:18:08 +0300

    Made ex04

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <iostream>
+
+#ifndef COLOR
+
+#define COLOR
+# define BACK_GRAY		"\033[47m"
+# define NORM			"\033[0m"
+# define BOLD			"\033[1m"
+# define RED			"\033[31m"
+# define GREEN			"\033[32m"
+# define YELLOW			"\033[33m"
+# define BLUE			"\033[34m"
+# define PURPLE			"\033[35m"
+# define BRIGHT_BLUE	"\033[36m"
+# define NONE			"\033[37m"
+
+#endif
+
+using	std::string;
+using	std::endl;
+using	std::cerr;
+using	std::cout;
+using	std::cin;
+
+int replaceStrInFiles(string fileName, string str1, string str2);
+
+int	main(int argc, char **argv)
+{
+	if (argc != 4 || !argv[1] || !argv[2] || !argv[3])
+	{
+		cerr << BOLD << RED
+			<< "Error!" << NORM
+			<< " You must use 3 arguments!\n"
+			<< BRIGHT_BLUE
+			<< "[1] - filename\n"
+			<< "[2] - string\n"
+			<< "[3] - string\n"
+			<< NORM
+			<< "and all of them must be"
+			<< BOLD << RED << " NOT " << NORM << "empty!" << endl;
+	}
+	else
+	{
+		if (!replaceStrInFiles(argv[1], argv[2], argv[3]))
+		{
+			cout << BOLD << RED
+				<< "Error in the process of replacing strings!" << NORM << endl;
+		}
+		else
+		{
+			cout << BOLD << GREEN
+				<< "Replacement has been ended successfully!" << NORM << endl;
+		}
+	}
+	return (0);
+}
---
+++ CPP01/ex04/replaceStrInFiles.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+
+# define BACK_GRAY		"\033[47m"
+# define NORM			"\033[0m"
+# define BOLD			"\033[1m"
+# define RED			"\033[31m"
+# define GREEN			"\033[32m"
+# define YELLOW			"\033[33m"
+# define BLUE			"\033[34m"
+# define PURPLE			"\033[35m"
+# define BRIGHT_BLUE	"\033[36m"
+# define NONE			"\033[37m"
+
+using std::string;
+using std::endl;
+using std::cerr;
+
+int replaceStrInFiles(string fileName, string str1, string str2)
+{
+	std::ifstream inputFile;
+	std::ofstream outputFile;
+	string	tmpStr;
+	size_t	start_pos;
+
+	inputFile.open(fileName);
+	if (inputFile.is_open())
+	{
+		outputFile.open(fileName.append(".replace"));
+		if (!outputFile.is_open())
+		{
+			cerr << BOLD << RED << "OutputFile open error!\n";
+			inputFile.close();
+			return (-1);
+		}
+		else
+		{
+			while (getline(inputFile, tmpStr))
+			{
+				while ((start_pos = tmpStr.find(str1, start_pos)) != string::npos)
+				{
+					tmpStr.erase(start_pos, str1.length());
+					tmpStr.insert(start_pos, str2);
+					start_pos += str2.length();
+				}
+				start_pos = 0;
+				outputFile << tmpStr << endl;
+			}
+		}
+	}
+	else
+	{
+		cerr << BOLD << RED << "InputFile open error!\n";
+		return (-1);
+	}
+	outputFile.close();
+	inputFile.close();
+	return (1);
+}
