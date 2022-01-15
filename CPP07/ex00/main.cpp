fadd6d25844202f5fd89c266dc5436cb00f8afed
vasekva <vahonin_vasek@mail.ru>
2022-01-15 19:35:18 +0300

    Finished ex00 of the Module 07

---
+++ CPP07/ex00/main.cpp
@@ -0,0 +1,18 @@
+#include "whatever.hpp"
+
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	cout << "a = " << a << ", b = " << b << endl;
+	cout << "min( a, b ) = " << ::min( a, b ) << endl;
+	cout << "max( a, b ) = " << ::max( a, b ) << endl;
+	string c = "chaine1";
+	string d = "chaine2";
+	::swap(c, d);
+	cout << "c = " << c << ", d = " << d << endl;
+	cout << "min( c, d ) = " << ::min( c, d ) << endl;
+	cout << "max( c, d ) = " << ::max( c, d ) << endl;
+	return 0;
+}
---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,25 @@
+#ifndef CPP_WHATEVER_HPP
+#define CPP_WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+using std::endl;
+using std::cout;
+using std::string;
+
+template <typename T>
+void	swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T	min(const T &a, const T &b) { return ((a < b) ? a : b); }
+
+template <typename T>
+T	max(const T &a, const T &b) { return ((a > b) ? a : b); }
+
+#endif
